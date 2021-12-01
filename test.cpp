43bd3ea3178619c2cf47e112757b8f797530a964
jike5 <2719825969@qq.com>
2021-12-01 15:31:00 +0800

    modified test.cpp#

--- test.cpp
+++ test.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
+#include<iomanip>
 #include<string>
-#include<vector>
-#include<algorithm>
+#include<map>
+#include<random>
+
 using namespace std;
 
 int main(){
-    vector<int> v;
-    for (int i = 0; i < 10; i++)
+    
+    std::random_device rd;
+    std::mt19937 e2(rd());
+    std::normal_distribution<> dist(0, 100);
+    
+    for(int i = 0; i < 100; i++)
     {
-        v.push_back(i);
+        double a = dist(e2);
+        a = a < -100 ? -100 : a;
+        a = a > 100 ? 100 : a;
+        std::cout <<"a" << i <<" = " << a << endl;
     }
-    for (auto p : v)
-    {
-        cout << p << endl;
-    }
+    
     system("pause");
     return 0;
 }
